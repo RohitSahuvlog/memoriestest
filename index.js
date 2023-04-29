d699c4f0ff873c0b279c4a71f71c2d42d59269fe
Rohit sahu <96680038+RohitSahuvlog@users.noreply.github.com>
2023-04-29 19:11:09 +0530

    t

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from "dotenv";
 import postRoutes from './routes/posts.js';
-
+dotenv.config();
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -16,10 +16,10 @@ app.get("/", (req, res) => {
 });
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
