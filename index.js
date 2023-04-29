ce13ca5a98d28414fec0cd60ebc3dd6131c9c1ce
Rohit sahu <96680038+RohitSahuvlog@users.noreply.github.com>
2023-04-29 18:55:51 +0530

    t

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-
+app.get("/", (req, res) => {
+  res.send({ 'message': "This is the Home" })
+});
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
